fb59b550f9789f9f4a1ac8f4369bc2037df85e33
ZimaBarskaya <32509577+ZimaBarskaya@users.noreply.github.com>
2018-01-10 17:48:22 +0200

    Update add.ts

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HomePage } from '../home/home';
 import { EditPage } from '../edit/edit';
 
@@ -10,18 +11,27 @@ import { EditPage } from '../edit/edit';
 export class AddPage {
   productList = [];
   filter = "";
-  listItemName = "Name";
-  constructor(public navCtrl: NavController, public navParams: NavParams) {//, public productList: any[], public filter: "", public listItemName: ""
+  listItemName = "";
+slideOneForm: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
     this.productList = navParams.get('productList');
     this.filter = navParams.get('filter');
+    this.slideOneForm = formBuilder.group({
+        listItemName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+    });
   }
   doneClick(){
-    this.productList.push({
-      type : ""+this.filter,
-      name : ""+this.listItemName
-    });
-    this.filter = "";
-    this.navCtrl.push(HomePage, {'productList' :this.productList, "filter" : this.filter });
+    if(!this.slideOneForm.valid){
+        console.log("omg");
+        $(".error").css("display", "block");
+    } else {
+      this.productList.push({
+        type : ""+this.filter,
+        name : ""+this.listItemName
+      });
+      this.filter = "";
+      this.navCtrl.push(HomePage, {'productList' :this.productList, "filter" : this.filter });
+    }
   }
 
   cancelClick(){
